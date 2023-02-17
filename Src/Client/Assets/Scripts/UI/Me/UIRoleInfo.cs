d65524b5e709112dfed67072ab946f50d7656402
Anigu <qwerwasd0917@163.com>
2023-02-17 22:25:02 +0800

    0217

---
+++ Src/Client/Assets/Scripts/UI/Me/UIRoleInfo.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Entities;
+using Models;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRoleInfo : MonoBehaviour
+{
+
+    public Slider hpSlider;
+    public Slider mpSlider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpSlider.value = User.Instance.CurrentCharacter.Attributes.MP / User.Instance.CurrentCharacter.Attributes.MaxHP;
+        mpSlider.value = User.Instance.CurrentCharacter.Attributes.MP / User.Instance.CurrentCharacter.Attributes.MaxMP;
+
+    }
+}
